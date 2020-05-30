5a954655749f3c24973f30d38922f40a3e28fff5
Pankaj Prakash <pankaj.prakash@vasudhaika.net>
2020-05-30 22:45:45 +0530

    Added login states and animations to login component

--- src/components/Auth/Login/Login.js
+++ src/components/Auth/Login/Login.js
@@ -1,9 +1,22 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { Button, Card, Form, Icon } from 'semantic-ui-react';
+import { Button, Card, Form, Icon, Popup } from 'semantic-ui-react';
 
 import classes from './Login.module.scss';
 
+/**
+ * 
+ * @param {{ 
+ *  onSubmit: () => {}, 
+ *  valueChangeHandler: (e, semanticEvent) => {},
+ *  userId: string,
+ *  password: string,
+ *  rememberMe: boolean,
+ *  progress: boolean,
+ *  emailError: { content: string, position: string }
+ *  passwordError: { content: string, position: string }
+ * }} props 
+ */
 const Login = (props) => {
 
   return (
@@ -13,39 +26,50 @@ const Login = (props) => {
           <Icon name="user circle outline" size="huge" color="teal" />
         </div>
 
-        <h2 className="mt-12">Log In</h2>
+        <h2 className="mt-0 mb-28">Login to your account</h2>
 
-        <Form onSubmit={props.onSubmit}>
+        <Form autoComplete="off" onSubmit={props.onSubmit}>
           <Form.Input
             focus
             type="email"
-            placeholder="Enter your email/user Id"
+            icon="mail outline"
+            placeholder="name@dashboard.com"
             width="16"
             name="userId"
             value={props.userId}
+            error={props.emailError}
             onChange={props.valueChangeHandler} />
 
           <Form.Input
             type="password"
-            placeholder="Enter your account password"
+            icon="key"
+            placeholder="Your secret password"
             width="16"
             name="password"
             value={props.password}
+            error={props.passwordError}
             onChange={props.valueChangeHandler} />
 
           <Form.Group className={classes.Remember}>
             <Form.Checkbox label="Remember Me" name="rememberMe" checked={props.rememberMe} onChange={props.toggleRememberMe} />
-            <a href="/">Forgot Password</a>
+
+            <Popup
+              content="Didn't remember your password? Click to reset your password!"
+              trigger={ <a href="/">Forgot Password?</a> }>
+            </Popup>
+              
           </Form.Group>
 
           <Button
             className="mt-28"
             fluid
+            loading={props.progress}
             content="Log In"
             color="teal"
             role="submit"
             type="submit" />
         </Form>
+
       </Card.Content>
     </Card>
   );
@@ -58,6 +82,7 @@ Login.propTypes = {
   password: PropTypes.string.isRequired,
   rememberMe: PropTypes.bool.isRequired,
   toggleRememberMe: PropTypes.func.isRequired,
+  progress: PropTypes.bool,
 };
 
 export default Login;
--- src/containers/Auth/LoginPage/LoginPage.js
+++ src/containers/Auth/LoginPage/LoginPage.js
@@ -1,26 +1,85 @@
 import React, { Component } from 'react';
+import { Transition } from 'semantic-ui-react';
 
 import Login from '../../../components/Auth/Login/Login';
 
 export default class LoginPage extends Component {
   state = {
-    userId: "",
-    password: "",
-    rememberMe: false,
+    loginDetails: {
+      userId: "",
+      password: "",
+      rememberMe: false,
+    },
+    visible: false,
+    loginProgress: false,
+    error: {
+      email: {
+        content: 'Please enter a valid email address',
+        pointing: 'below',
+      },
+      password: {
+        content: 'Password is required',
+        pointing: 'above',
+      }
+    },
+    showError: {
+      email: false,
+      password: false,
+    },
   };
+
+  componentDidMount() {
+    this.showLoginComponent();
+  }
+
+  /**
+   * Shows the login component after delay.
+   * 
+   * This delay is essential for semantic UI transition animation.
+   */
+  showLoginComponent = () => {
+    setTimeout(() => this.setState({ visible: true }), 50);
+  }
 
   /**
    * Input fields value change handler
    */
   valueChangeHandler = (e, { name, value }) => {
-    this.setState({ [name]: value });
+
+    const loginDetails = { ...this.state.loginDetails };
+
+    loginDetails[name] = value;
+
+    this.setState({ loginDetails });
   };
 
   /**
    * Toggles remember me
    */
   toggleRememberMe = () => {
-    this.setState(prevState => ({ rememberMe: !prevState.rememberMe}));
+
+    this.setState(prevState => {
+      const loginDetails = { ...this.state.loginDetails };
+  
+      loginDetails.rememberMe = !prevState.loginDetails.rememberMe;
+
+      return { loginDetails: loginDetails };
+    });
+  }
+
+  /**
+   * Sets login progress mode to true.
+   * 
+   * @param {boolean} val
+   */
+  setLoginProgress = (val) => {
+    this.setState({ loginProgress: val })
+  }
+
+  /**
+   */
+  setLoginErrors = () => {
+    this.setState((prevState) => ({ showError: { email: true, password: true }}));
   }
 
   /**
@@ -28,19 +87,35 @@ export default class LoginPage extends Component {
    */
   loginHandler = () => {
     console.log(this.state);
+
+    this.setLoginProgress(true);
+
+    this.setLoginErrors();
   };
+
 
   render() {
     return (
-      <Login
-        {...this.props}
-        onSubmit={this.loginHandler}
-        valueChangeHandler={this.valueChangeHandler}
-        userId={this.state.userId}
-        password={this.state.password}
-        rememberMe={this.state.rememberMe}
-        toggleRememberMe={this.toggleRememberMe}
-      />
+
+      <Transition visible={this.state.visible} animation="fade up" duration={600}>
+
+        {/* Without container transition does not work for custom components */}
+        <div>
+          <Login
+            {...this.props}
+            onSubmit={this.loginHandler}
+            valueChangeHandler={this.valueChangeHandler}
+            userId={this.state.loginDetails.userId}
+            password={this.state.loginDetails.password}
+            rememberMe={this.state.loginDetails.rememberMe}
+            toggleRememberMe={this.toggleRememberMe}
+            progress={this.state.loginProgress}
+            emailError={this.state.showError.email ? this.state.error.email : null}
+            passwordError={this.state.showError.password ? this.state.error.password : null}
+          />
+        </div>
+
+      </Transition>
     );
   }
 }
