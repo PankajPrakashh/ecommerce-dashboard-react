79981bbe55088d69eca1e932514cff885644d4fe
Pankaj Prakash <pankaj.prakash@vasudhaika.net>
2020-05-30 12:34:14 +0530

    Added semantic-ui CSS framework

--- src/containers/Auth/LoginPage/LoginPage.js
+++ src/containers/Auth/LoginPage/LoginPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
+import { Button } from 'semantic-ui-react';
 
-import RaisedButton from '../../../components/UI/Buttons/RaisedButton/RaisedButton';
 import classes from './LoginPage.module.scss';
 
 export default (props) => {
@@ -9,24 +9,7 @@ export default (props) => {
     <div className={classes.LoginPage}>
       Login works
 
-      <br />
-      <br />
-      
-      <RaisedButton type="button" title="Click to login" color="accent">
-        Click me {/* <i className="fa fa-users"></i> */}
-      </RaisedButton>
-      <br />
-      <br />
-      
-      <RaisedButton type="button" title="Click to login" color="warn">
-        Click me {/* <i className="fa fa-users"></i> */}
-      </RaisedButton>
-      <br />
-      <br />
-      
-      <RaisedButton type="button" title="Click to login" color="primary">
-        Click me {/* <i className="fa fa-users"></i> */}
-      </RaisedButton>
+      <Button>Hello</Button>
     </div>
   );
 }
